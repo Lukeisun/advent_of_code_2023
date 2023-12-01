18d5bcd6c7b66cca41953d01f88a04b2901d087f
lukeisun <lukeisun57@gmail.com>
2023-12-01 00:12:15 -0500

    part 1 is done, string manipulation is rough in rust for part 2 :(

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,12 +3,48 @@ use std::{
     io::{self, Read},
     process::exit,
 };
-
-fn part2(input: &str) -> &str {
-    todo!();
+#[derive(Debug)]
+struct NumMapping {
+    str_repr: String,
+    num: u32,
 }
-fn part1(input: &str) -> &str {
-    todo!();
+fn part2(input: &str) -> String {
+    let mut count = 0;
+    let mut num_map: Vec<NumMapping> = Vec::new();
+    let str_repr_arr = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    for (i, str_repr) in str_repr_arr.iter().enumerate() {
+        num_map.push(NumMapping {
+            str_repr: str_repr.to_string(),
+            num: (i as u32) + 1,
+        });
+    }
+    for line in input.split("\n").filter(|x| !x.is_empty()) {
+        let mut copy = line.to_string();
+        for s in str_repr_arr.iter() {
+            copy = copy.replace(s, format!(",{},", s).as_str());
+        }
+
+        println!("{:?}", copy);
+    }
+    todo!()
+    // count.to_string()
+}
+fn part1(input: &str) -> String {
+    let mut count = 0;
+    for line in input.split("\n").filter(|x| !x.is_empty()) {
+        let mut temp = String::new();
+        let line: Vec<char> = line
+            .chars()
+            .into_iter()
+            .filter(|x| x.to_digit(10).is_some())
+            .collect();
+        temp.push(*line.first().unwrap());
+        temp.push(*line.last().unwrap());
+        count += temp.parse::<u32>().unwrap();
+    }
+    count.to_string()
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
