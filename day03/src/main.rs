993b2c643a150c9d82707a2467e2acaf15add1bc
lukeisun <lukeisun57@gmail.com>
2023-12-03 12:50:34 -0500

    ok you can read this code now

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -5,64 +5,61 @@ use std::{
     process::exit,
 };
 #[derive(Debug)]
-enum Symbol {
+enum Token {
     Digit(u8),
-    Character(char),
-    Period(char),
-}
-#[derive(Debug)]
-enum SymbolG {
-    Digit(u8),
-    Character(char),
-    Period(char),
-    Gear(char),
+    Symbol,
+    Period,
+    Gear,
 }
 
 fn part2(input: &str) -> String {
-    let mut schematic: Vec<Vec<SymbolG>> = Vec::new();
+    let mut schematic: Vec<Vec<Token>> = Vec::new();
     let mut sum = 0;
     for line in lines(input) {
-        let row = line
+        let mut row: Vec<Token> = line
             .chars()
             .into_iter()
             .map(|x| match x.to_digit(10) {
-                Some(symbol) => SymbolG::Digit(symbol as u8),
+                Some(symbol) => Token::Digit(symbol as u8),
                 _ => match x {
-                    '.' => SymbolG::Period(x),
-                    '*' => SymbolG::Gear(x),
-                    _ => SymbolG::Character(x),
+                    '.' => Token::Period,
+                    '*' => Token::Gear,
+                    _ => Token::Symbol,
                 },
             })
             .collect();
+        row.push(Token::Period);
         schematic.push(row);
     }
+    let mut gears_map = HashMap::new();
     let mut map = HashMap::new();
     let schematic = &*schematic;
     // get digits and map to idxs
     for (i, row) in schematic.into_iter().enumerate() {
         let mut temp: String = String::new();
         let mut indexs: Vec<(usize, usize)> = Vec::new();
+        let mut starting_index: (usize, usize) = (0, 0);
         for (j, col) in row.into_iter().enumerate() {
             match col {
-                SymbolG::Digit(x) => {
+                Token::Digit(x) => {
+                    if temp.is_empty() {
+                        starting_index = (i, j);
+                    }
                     indexs.push((i, j));
                     temp.push(char::from_digit(*x as u32, 10).unwrap());
                 }
                 _ => {
                     if let Ok(value) = temp.parse::<u32>() {
                         for index in indexs.clone().into_iter() {
-                            map.insert(index, value);
+                            map.insert(index, (value, starting_index));
                         }
                         indexs = Vec::new();
                         temp.clear();
                     }
+                    if let Token::Gear = col {
+                        gears_map.insert((i, j), ());
+                    }
                 }
-            }
-        }
-        // in case last col
-        if let Ok(value) = temp.parse::<u32>() {
-            for index in indexs.clone().into_iter() {
-                map.insert(index, value);
             }
         }
     }
@@ -78,116 +75,73 @@ fn part2(input: &str) -> String {
     ];
     let row_len = schematic.len();
     let col_len = schematic[0].len();
-    for (i, row) in schematic.into_iter().enumerate() {
-        for (j, col) in row.into_iter().enumerate() {
-            if let SymbolG::Gear(_) = col {
-                let mut first_match: Option<u32> = None;
-                let mut first_match_idx = (std::usize::MAX, std::usize::MAX);
-                let mut second_match: Option<u32> = None;
-                let mut second_match_idx = (std::usize::MAX, std::usize::MAX);
-                for direction in dir_array {
-                    let dir_i = i as i32 + direction.0;
-                    let dir_j = j as i32 + direction.1;
-                    if dir_i >= row_len as i32 || dir_j >= col_len as i32 || dir_i < 0 || dir_j < 0
-                    {
-                        continue;
-                    }
-                    let t: (usize, usize) = (dir_i as usize, dir_j as usize);
-                    if map.contains_key(&t) {
-                        if !first_match.is_some() {
-                            first_match = map.get(&t).copied();
-                            first_match_idx = t;
-                        } else if !second_match.is_some() {
-                            // println!("{}", first_match.unwrap());
-                            let temp = map.get(&t).copied();
-                            let f_i = first_match_idx.0;
-                            let f_j = first_match_idx.1;
-                            if f_i == t.0 {
-                                let distance = (f_j as i32 - t.1 as i32).abs();
-                                // println!("{}", distance);
-                                if distance == 2 {
-                                    let middle = f_j.max(t.1) - 1;
-                                    // println!("{} {}", f_i, middle);
-                                    if !map.contains_key(&(f_i, middle)) {
-                                        second_match = temp;
-                                        second_match_idx = t;
-                                    }
-                                }
-                                continue;
-                            }
-                            second_match_idx = t;
-                            second_match = temp;
-                        } else {
-                            let f_i = first_match_idx.0;
-                            let f_j = first_match_idx.1;
-                            let s_i = second_match_idx.0;
-                            let s_j = second_match_idx.1;
-                            if f_i == t.0 {
-                                let distance = (f_j as i32 - t.1 as i32).abs();
-                                if distance == 2 {
-                                    let middle = f_j.max(t.1) - 1;
-                                    if !map.contains_key(&(f_i, middle)) {
-                                        continue;
-                                    }
-                                } else {
-                                    continue;
-                                }
-                            }
-                            if s_i == t.0 {
-                                let distance = (s_j as i32 - t.1 as i32).abs();
-                                if distance == 2 {
-                                    let middle = s_j.max(t.1) - 1;
-                                    if !map.contains_key(&(s_i, middle)) {
-                                        continue;
-                                    }
-                                } else {
-                                    continue;
-                                }
-                            }
-                            first_match = None;
-                            second_match = None;
-                            break;
-                        }
-                    }
+    for (gear_idx, _) in gears_map {
+        let mut first_match: Option<(u32, (usize, usize))> = None;
+        let mut second_match: Option<(u32, (usize, usize))> = None;
+        for dir in dir_array {
+            let di = dir.0 + gear_idx.0 as i32;
+            let dj = dir.1 + gear_idx.1 as i32;
+            if dj >= col_len as i32 || di >= row_len as i32 || di < 0 || dj < 0 {
+                continue;
+            }
+            let curr = (di as usize, dj as usize);
+            if !map.contains_key(&curr) {
+                continue;
+            }
+            if !first_match.is_some() {
+                first_match = map.get(&curr).copied();
+            } else if !second_match.is_some() {
+                let temp_option = map.get(&curr).copied();
+                let temp = temp_option.unwrap();
+                let first_match_idx = first_match.unwrap().1;
+                let curr_idx = temp.1;
+                if curr_idx == first_match_idx {
+                    continue;
                 }
-                if first_match.is_some() && second_match.is_some() {
-                    println!("{} {}", first_match.unwrap(), second_match.unwrap());
-                    sum += first_match.unwrap() * second_match.unwrap();
+                second_match = temp_option;
+            } else {
+                let first = first_match.unwrap();
+                let second = second_match.unwrap();
+                if first.1 == second.1 {
+                    first_match = None;
+                    second_match = None;
+                    break;
                 }
             }
+        }
+        if first_match.is_some() && second_match.is_some() {
+            sum += first_match.unwrap().0 * second_match.unwrap().0;
         }
     }
     sum.to_string()
 }
 fn part1(input: &str) -> String {
-    let mut schematic: Vec<Vec<Symbol>> = Vec::new();
+    let mut schematic: Vec<Vec<Token>> = Vec::new();
     for line in lines(input) {
-        let row = line
+        let mut row: Vec<Token> = line
             .chars()
             .into_iter()
             .map(|x| match x.to_digit(10) {
-                Some(symbol) => Symbol::Digit(symbol as u8),
+                Some(token) => Token::Digit(token as u8),
                 _ => match x {
-                    '.' => Symbol::Period(x),
-                    _ => Symbol::Character(x),
+                    '.' => Token::Period,
+                    _ => Token::Symbol,
                 },
             })
             .collect();
+        row.push(Token::Period);
         schematic.push(row);
     }
-    // println!("{:#?}", schematic);
     let mut count = 0;
     let dir_array: [(i32, i32); 8] = [
         (1, 0),
         (-1, 0),
-        (0, -1),
-        (1, -1),
-        (-1, -1),
-        // these are last because if we move beyond the column len
-        // there is no next symbol to read. cant handle if its a part etc
-        (1, 1),
         (0, 1),
+        (0, -1),
+        (1, 1),
+        (1, -1),
         (-1, 1),
+        (-1, -1),
     ];
     let row_len = schematic.len();
     let col_len = schematic[0].len();
@@ -197,34 +151,25 @@ fn part1(input: &str) -> String {
         let mut is_part: bool = false;
         for (j, col) in row.into_iter().enumerate() {
             match col {
-                Symbol::Period(_) | Symbol::Character(_) => {
+                Token::Digit(x) => {
+                    temp.push(char::from_digit(*x as u32, 10).unwrap());
+                    for dir in dir_array {
+                        let di = i as i32 + dir.0;
+                        let dj = j as i32 + dir.1;
+                        if di >= row_len as i32 || dj >= col_len as i32 || di < 0 || dj < 0 {
+                            continue;
+                        }
+                        if let Token::Symbol = &schematic[di as usize][dj as usize] {
+                            is_part = true;
+                        }
+                    }
+                }
+                _ => {
                     if !temp.is_empty() && is_part {
                         count += temp.parse::<u32>().unwrap();
                     }
                     is_part = false;
                     temp.clear();
-                }
-                Symbol::Digit(x) => {
-                    temp.push(char::from_digit(*x as u32, 10).unwrap());
-                    for direction in dir_array {
-                        let dir_i = i as i32 + direction.0;
-                        let dir_j = j as i32 + direction.1;
-                        if dir_j >= col_len as i32 {
-                            if !temp.is_empty() && is_part {
-                                count += temp.parse::<u32>().unwrap();
-                            }
-                            is_part = false;
-                            temp.clear();
-                            break;
-                        }
-                        if dir_i >= row_len as i32 || dir_i < 0 || dir_j < 0 {
-                            continue;
-                        }
-                        let symbol = &schematic[dir_i as usize][dir_j as usize];
-                        if let Symbol::Character(_) = symbol {
-                            is_part = true;
-                        }
-                    }
                 }
             }
         }
