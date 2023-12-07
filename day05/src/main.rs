081babdd71a9a7e53afc61065880f854ce4d71be
lukeisun <lukeisun57@gmail.com>
2023-12-06 19:59:18 -0500

    It works on test_input at least, not the actual input :(

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -24,20 +24,18 @@ fn part2(input: &str) -> String {
         });
     }
     // println!("{:#?}", seeds);
-    let mut u_to_v_map: Vec<(String, HashMap<Range<u64>, Range<u64>>)> = Vec::new();
-    let mut u_to_v = String::new();
+    let mut u_to_v_map: Vec<HashMap<Range<u64>, Range<u64>>> = Vec::new();
     let mut map = HashMap::new();
     for block in blocks.into_iter().skip(1) {
         if block.is_empty() {
             if map.is_empty() {
                 continue;
             }
-            u_to_v_map.push((u_to_v.clone(), map));
+            u_to_v_map.push(map);
             map = HashMap::new();
             continue;
         }
         if block.contains(":") {
-            u_to_v = block.split_whitespace().nth(0).unwrap().to_string();
             continue;
         }
         let nums: Vec<u64> = block
@@ -57,207 +55,64 @@ fn part2(input: &str) -> String {
         };
         map.insert(src_range, dest_range);
     }
-    println!("{:#?}", u_to_v_map);
-    let mut seed_to: Vec<(Range<u64>, Range<u64>)> =
-        seeds.clone().into_iter().map(|x| (x.clone(), x)).collect();
-    let u_to_v_map = &*u_to_v_map;
-    for map in u_to_v_map.into_iter().take(2) {
-        let mut temp = Vec::new();
-        println!("{:?}", map);
-        for (seed, to) in seed_to.clone() {
-            println!("\nseed: {:?}\nto: {:?}\n", seed, to);
-            for (key, val) in &map.1 {
-                let contains_start = key.contains(&to.start);
-                let contains_end = key.contains(&(to.end - 1));
-                println!("\nkey {:?}\nval {:?}", key, val);
-                if contains_start && contains_end {
+    println!("{:?}", u_to_v_map);
+    // let mut ranges: Vec<Range<u64>> = seeds.clone();
+    let mut ranges = HashMap::new();
+    for seed in seeds {
+        ranges.insert(seed, ());
+    }
+    for map in u_to_v_map.into_iter() {
+        // println!("{:?}", map);
+        let mut temp = HashMap::new();
+        // println!("{:?}", ranges);
+        for (range, _) in &ranges {
+            let mut handled = false;
+            for (src, dest) in &map {
+                let contains_start = src.contains(&range.start);
+                let contains_end = src.contains(&(range.end - 1));
+                if contains_end && contains_start {
                     dbg!("BOTH CONTAINS");
-                    let start_diff = to.start - key.start;
-                    let end_diff = key.end - to.end;
-                    let new_to_val = val.start + start_diff..val.end - end_diff;
-                    let new_to = (seed.clone(), new_to_val);
-                    println!("{:?}", new_to);
-                    temp.push(new_to);
-                    let left_potential_seed = key.start..to.start;
-                    let left_potential = (left_potential_seed, val.start..val.start + start_diff);
-                    // temp.push(left_potential);
-                    let right_potential_seed = to.end..key.end;
-                    let right_potential = (right_potential_seed, val.end - end_diff..val.end);
-                    // temp.push(right_potential);
+                    println!("src: {:?} - dest: {:?} - range - {:?}", src, dest, range);
+                    println!("{:?}", temp);
+                    let start_diff = range.start - src.start;
+                    let range_diff = range.end - range.start;
+                    println!(
+                        "{:?} range_diff {} start_diff {}",
+                        src.start + start_diff..src.start + start_diff + range_diff,
+                        range_diff,
+                        start_diff
+                    );
+                    temp.insert(
+                        dest.start + start_diff..dest.start + start_diff + range_diff,
+                        (),
+                    );
+                    println!("{:?}", temp);
+                    handled = true;
                 } else if contains_end && !contains_start {
                     dbg!("END CONTAINS");
-                    // src/dst = key/val
-                    // seed/val = seed/to
-                    let split = key.start;
-                    // seed range
-                    let f_val_split = to.start..split;
-                    // new range
-                    let s_val_split = split..to.end;
-                    println!("val {:?} {:?}", f_val_split, s_val_split);
-                    let diff = s_val_split.end - s_val_split.start;
-                    let seed_split = seed.start + (split - val.start);
-                    let f_seed_split = seed.start..seed_split;
-                    let s_seed_split = seed_split..seed.end;
-                    let first = (f_seed_split, f_val_split);
-                    println!("first {:?}", first);
-                    temp.push(first);
-                    let second = (s_seed_split, val.start..val.start + diff);
-                    println!("second {:?}", second);
-                    temp.push(second);
-                // let diff = .end - split;
-                // let original =
+                    temp.insert(range.start..src.start, ());
+                    let diff = range.end - src.start;
+                    temp.insert(dest.start..dest.start + diff, ());
+                    handled = true;
                 } else if contains_start {
                     dbg!("START CONTAINS");
-                } else {
-                    dbg!("NO CONTAINS");
-                    temp.push((key.clone(), val.clone()));
+                    temp.insert(src.end..range.end, ());
+                    let start_diff = range.start - src.start;
+                    temp.insert(dest.start + start_diff..dest.end, ());
+                    handled = true;
                 }
             }
+            if !handled {
+                temp.insert(range.clone(), ());
+            }
         }
-        seed_to = temp;
-        println!("{:#?}", seed_to);
+        ranges = temp;
+        // println!("{:#?}", ranges);
     }
-    // seeds.push(13..13);
-    // for m in u_to_v_map {
-    //     let mut temp = Vec::new();
-    //     println!("m {:?}", m);
-    //     println!("{:?}", seeds_unravel);
-    //     for s in &seeds_unravel {
-    //         let seed = &s.0;
-    //         let value = &s.1;
-    //         println!("seed: {:?}\nvalue: {:?}", seed, value);
-    //         let curr_map = &m.1;
-    //         let full_contains = curr_map
-    //             .into_iter()
-    //             .filter(|x| x.0.contains(&value.end) && x.0.contains(&value.start))
-    //             .map(|x| x)
-    //             .reduce(|_, range| range);
-    //         }
-    //         let start_contains = curr_map
-    //             .into_iter()
-    //             .filter(|x| !x.0.contains(&value.end) && x.0.contains(&value.start))
-    //             .map(|x| x)
-    //             .reduce(|_, range| range);
-    //         if let Some(mapping) = start_contains {
-    //             println!("START");
-    //             let src_range = mapping.0;
-    //             let dst_range = mapping.1;
-    //             let split = src_range.end;
-    //             println!("src {:?} dst {:?} {split}", src_range, dst_range);
-    //             let first_value_split = Range {
-    //                 start: value.start,
-    //                 end: split,
-    //             };
-    //             let rest_value_split = Range {
-    //                 start: split,
-    //                 end: value.end,
-    //             };
-    //             let diff = first_value_split.end - first_value_split.start;
-    //             let seed_split = seed.start + diff;
-    //             let first_seed_split = Range {
-    //                 start: seed.start,
-    //                 end: seed_split,
-    //             };
-    //             let rest_seed_split = Range {
-    //                 start: seed_split,
-    //                 end: seed.end,
-    //             };
-    //             let rest_split = (rest_seed_split, rest_value_split);
-    //             let first_split = (
-    //                 first_seed_split,
-    //                 Range {
-    //                     start: dst_range.end - diff,
-    //                     end: dst_range.end,
-    //                 },
-    //             );
-    //             // let more_seeds = (
-    //             //     Range {
-    //             //         start: src_range.start,
-    //             //         end: seed.start,
-    //             //     },
-    //             //     Range {
-    //             //         start: dst_range.start,
-    //             //         end: dst_range.start + (seed.start - src_range.start),
-    //             //     },
-    //             // );
-    //             println!(
-    //                 "first {:?} second {:?} diff {:?} ",
-    //                 first_split, rest_split, diff
-    //             );
-    //             // temp.push(more_seeds);
-    //             temp.push(first_split);
-    //             temp.push(rest_split);
-    //             continue;
-    //         }
-    //         let end_contains = curr_map
-    //             .into_iter()
-    //             .filter(|x| x.0.contains(&value.end) && !x.0.contains(&value.start))
-    //             .map(|x| x)
-    //             .reduce(|_, range| range);
-    //         if let Some(mapping) = end_contains {
-    //             let src_range = mapping.0;
-    //             let dst_range = mapping.1;
-    //             let split = src_range.start;
-    //             let first_value_split = Range {
-    //                 start: value.start,
-    //                 end: split,
-    //             };
-    //             let second_value_split = Range {
-    //                 start: split,
-    //                 end: value.end,
-    //             };
-    //             let diff = second_value_split.end - second_value_split.start;
-    //             let seed_split = seed.end - diff;
-    //             let first_seed_split = Range {
-    //                 start: seed.start,
-    //                 end: seed_split,
-    //             };
-    //             let second_seed_split = Range {
-    //                 start: seed_split,
-    //                 end: seed.end,
-    //             };
-    //             let first_split = (first_seed_split, first_value_split);
-    //             let second_split = (
-    //                 second_seed_split,
-    //                 Range {
-    //                     start: dst_range.start,
-    //                     end: dst_range.start + diff,
-    //                 },
-    //             );
-    //             temp.push(first_split);
-    //             temp.push(second_split);
-    //             continue;
-    //         }
-    //         println!("s - {:?}", s);
-    //         temp.push(s.clone());
-    //     }
-    //     println!("temp {:?}", temp);
-    //     seeds_unravel = temp;
-    //     println!("seeds {:#?}", seeds_unravel.clone());
-    // }
-    // println!("{:#?}", seeds_unravel);
+    let mut ranges: Vec<u64> = ranges.into_keys().map(|x| x.start as u64).collect();
+    ranges.sort();
+    println!("{:#?}\n{}", ranges, ranges[0]);
     "pog".to_string()
-    // let mut locations = Vec::new();
-    // for s in seeds {
-    //     for seed in s {
-    //         let mut source = seed;
-    //         for m in u_to_v_map {
-    //             let curr_map = &m.1;
-    //             let range = curr_map
-    //                 .into_iter()
-    //                 .filter(|x| x.0.contains(&source))
-    //                 .map(|x| x.0)
-    //                 .reduce(|_, range| range);
-    //             if let Some(range) = range {
-    //                 let diff = source - range.start;
-    //                 let dest = curr_map.get(&range).unwrap();
-    //                 source = dest.start + diff;
-    //             }
-    //         }
-    //         locations.push(source);
-    //     }
-    // }
-    // locations.into_iter().min().unwrap().to_string()
 }
 fn part1(input: &str) -> String {
     // the destination range start, the source range start, and the range length.
